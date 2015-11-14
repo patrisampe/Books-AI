c7c44c517a94db8bbca31142211f37849132b2fb
yoel.cabo <yoel.cabo@est.fib.upc.edu>
2015-11-14 23:32:33 +0100

    mejorando script

--- CrearClp.sh
+++ CrearClp.sh
@@ -1,1 +1,12 @@
-cat $(cat FitxersEnOrdre.txt) > Libros.clp
+CLP=Libros.clp
+rm $CLP 
+touch $CLP
+for fit in $(cat FitxersEnOrdre.txt); do
+    echo ";;; $fit BEGIN" >> $CLP
+    echo "" >> $CLP 
+    cat $fit >> $CLP 
+    echo "" >> $CLP
+    echo ";;; $fit END" >> $CLP
+    echo "" >> $CLP
+    echo "" >> $CLP
+done
