c55cf8033a2456ea259af717f4d3de6f4b30296f
yoel.cabo <yoel.cabo@est.fib.upc.edu>
2015-11-14 23:42:07 +0100

    arasi

--- CrearClp.sh
+++ CrearClp.sh
@@ -1,12 +1,15 @@
 CLP=Libros.clp
 rm $CLP 
 touch $CLP
-for fit in $(ls $(cat FitxersEnOrdre.txt)); do
-    echo ";;; $fit BEGIN" >> $CLP
-    echo "" >> $CLP 
-    cat $fit >> $CLP 
-    echo "" >> $CLP
-    echo ";;; $fit END" >> $CLP
-    echo "" >> $CLP
-    echo "" >> $CLP
+for name in $(cat FitxersEnOrdre.txt); do
+    for fit in $(ls $name); do
+        echo "Añadiendo $fit a $CLP ..."
+	echo ";;; $fit BEGIN" >> $CLP
+	echo "" >> $CLP 
+	cat $fit >> $CLP 
+	echo "" >> $CLP
+	echo ";;; $fit END" >> $CLP
+	echo "" >> $CLP
+	echo "" >> $CLP
+    done
 done
