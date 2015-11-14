e915bb4def367e2824a6372153a02bcbf0a4a6f0
yoel.cabo <yoel.cabo@est.fib.upc.edu>
2015-11-14 23:56:15 +0100

    Script bonito y todo

--- CrearClp.sh
+++ CrearClp.sh
@@ -6,7 +6,14 @@ CLP=Libros.clp
 if [ $# -eq 1 ]; then
     CLP=$1
 fi
-cat /dev/null > $CLP
+echo ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;" > $CLP
+echo ";;          Practica 2 IA       ;;" >> $CLP
+echo ";; Por:                         ;;" >> $CLP
+echo ";; * Octavi Pascual             ;;" >> $CLP
+echo ";; * Patricia Sampedro          ;;" >> $CLP
+echo ";; * Yoel Cabo                  ;;" >> $CLP
+echo ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;" >> $CLP
+echo "" >> $CLP
 for name in $(cat FitxersEnOrdre.txt); do
     for fit in $(ls $name); do
         echo "Añadiendo $fit a $CLP ..."
