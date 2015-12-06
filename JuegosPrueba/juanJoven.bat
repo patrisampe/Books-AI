c1da2cf6131f09c0e6ba601dec6225fa131a4071
octavi.pascual <octavi.pascual@est.fib.upc.edu>
2015-12-06 17:45:57 +0100

    Juan Joven .bat

---
+++ JuegosPrueba/juanJoven.bat
@@ -0,0 +1,20 @@
+(clear)
+(load "../Libros.clp")
+(reset)
+
+(assert (Lector (nombre "Juan Joven") (edad 15) (sexo hombre) (nacionalidad [libros_Class3]) (estudios No_graduado_escolar)))
+(assert (Habitos (lugar cama) (frecuencia diario) (momento tarde) (tiempo 60)))
+(assert 
+	(Preferencias
+		(autores-preferidos [Libros_Class10051] [Libros_Class10054] [Libros_Class10056] [libros_Class58])
+		(generos-preferidos [libros_Class53])
+		(temas-preferidos [libros_Class38])
+		(autores-deseados [Libros_Class10059])
+	)
+)
+(assert
+	(Prohibiciones
+		(autores-prohibidos [Libros_Class10063])
+		(temas-prohibidos [libros_Class68] [libros_Class39])
+	)
+)
